6cbd68708a19ac9761a438c8c3dec9a3387c61dc
emvh <emilyvho@gmail.com>
2020-06-22 19:53:36 -0700

    Test passing props from App to Carousel and render 1 photo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,43 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 import Carousel from './Carousel.jsx';
+import sampleData from '../sampleData.js'
 
 console.log('hello app');
 
 class App extends React.Component {
-  constructor () {
-    super();
+  constructor (props) {
+    super(props);
     this.state = {
-      hotel: [],
+      hotel: sampleData,
       preview: []
     }
   }
 
-  componentDidMount() {
-    this.getData();
-  }
+  // componentDidMount() {
+  //   this.getData();
+  // }
 
-  // GET
-  // testing with 1 hotel
-  getData() {
-    axios.get('/api/photos')
-      .then( response => {
-        const hotel = response.data;
-        this.setState({
-          hotel: hotel[0],
-          preview: [hotel[0]['roomAlbum'][0], hotel[0]['diningAlbum'][0], hotel[0]['poolAlbum'][0], hotel[0]['gymAlbum'][0]]
-        });
-        console.log('res data', this.state.hotel);
-        console.log('preview data', this.state.preview);
-        error => console.log(error);
-      })
-  }
+  // // GET
+  // // testing with 1 hotel
+  // getData() {
+  //   axios.get('/api/photos')
+  //     .then( response => {
+  //       const hotel = response.data;
+  //       this.setState({
+  //         hotel: hotel[0],
+  //         preview: [hotel[0]['roomAlbum'][0], hotel[0]['diningAlbum'][0], hotel[0]['poolAlbum'][0], hotel[0]['gymAlbum'][0]]
+  //       });
+  //       console.log('res data', this.state.hotel);
+  //       console.log('preview data', this.state.preview);
+  //       error => console.log(error);
+  //     })
+  // }
 
   render() {
     return (
       <div>
         <span>Hello App</span>
-        <Carousel photos={this.state.preview} />
+        <div><Carousel photos={this.state.hotel} /></div>
       </div>
     )
   }
--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -1,32 +1,59 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
 
 console.log('carousel');
 
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      currentPhotoIndex: 0
-    }
   }
 
-  previousPhoto() {
-
-  }
-
-  render () {
-    console.log('image1', this.props.photos[0]);
-    // console.log('image2', this.props.photos[0]['caption']);
+  render() {
+    // console.log('image', this.props.photos[0]['diningAlbum'][0]['imageUrl']);
 
     return (
-      <div className="carousel">
-        <span>Carousel</span>
-        <img src={this.props.photos[0]['imageUrl']} />
+      <div>
+        <img src={this.props.photos[0]['diningAlbum'][0]['imageUrl']}/>
       </div>
-    );
+    )
   }
+
 }
 
-
 export default Carousel;
+
+
+
+
+// import React from 'react';
+// // import ReactDOM from 'react-dom';
+
+// console.log('carousel');
+
+// class Carousel extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       currentPhotoIndex: 0
+//     }
+//   }
+
+//   // previousPhoto() {
+
+//   // }
+
+//   render () {
+//     {console.log('image1', this.props.photos[0])['diningAlbum'][0]['imageUrl']}
+//     // console.log('image2', this.props.photos[0]['userAvatarURL']);
+
+//     return (
+//       <div>
+//         <span>Carousel</span>
+//         <div>test</div>
+//         <img src={"https://images.unsplash.com/photo-1466978913421-dad2ebd01d17?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjE0Mjk4NH0"} />
+//       </div>
+//     );
+//   }
+// }
+
+
+// export default Carousel;
