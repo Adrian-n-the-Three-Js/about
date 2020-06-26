64fb1629c8643efb13794cc22c9726ed1704cf0e
emvh <emilyvho@gmail.com>
2020-06-25 17:50:15 -0700

    Adjust seeding script to grab more albums and include additional properties

--- database/index.js
+++ database/index.js
@@ -4,13 +4,14 @@ mongoose.connect('mongodb://localhost/hotel', {useNewUrlParser: true, useUnified
 // test connection
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.once('open', () => {
   console.log('DATABASE Connected!');
 });
 
 // create schema
 // native mongo object id will represent hotel id
 const photoSchema = new mongoose.Schema({
+  hotelPrice: Number,
   roomAlbum: [
     {
       user: String,
@@ -19,8 +20,10 @@ const photoSchema = new mongoose.Schema({
       caption: String,
       category: String,
       datePosted: String,
-      helpfulVotes: Number
-    }
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
   ],
   diningAlbum: [
     {
@@ -30,8 +33,10 @@ const photoSchema = new mongoose.Schema({
       caption: String,
       category: String,
       datePosted: String,
-      helpfulVotes: Number
-    }
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
   ],
   poolAlbum: [
     {
@@ -41,8 +46,10 @@ const photoSchema = new mongoose.Schema({
       caption: String,
       category: String,
       datePosted: String,
-      helpfulVotes: Number
-    }
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
   ],
   gymAlbum: [
     {
@@ -52,9 +59,63 @@ const photoSchema = new mongoose.Schema({
       caption: String,
       category: String,
       datePosted: String,
-      helpfulVotes: Number
-    }
-  ]
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
+  ],
+  amenitiesAlbum: [
+    {
+      user: String,
+      userAvatarURL: String,
+      imageUrl: String,
+      caption: String,
+      category: String,
+      datePosted: String,
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
+  ],
+  bathroomAlbum: [
+    {
+      user: String,
+      userAvatarURL: String,
+      imageUrl: String,
+      caption: String,
+      category: String,
+      datePosted: String,
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
+  ],
+  eventRoomAlbum: [
+    {
+      user: String,
+      userAvatarURL: String,
+      imageUrl: String,
+      caption: String,
+      category: String,
+      datePosted: String,
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
+  ],
+  roomViewAlbum: [
+    {
+      user: String,
+      userAvatarURL: String,
+      imageUrl: String,
+      caption: String,
+      category: String,
+      datePosted: String,
+      helpfulVotes: Number,
+      location: String,
+      contributions: Number,
+    },
+  ],
 });
 
 // compile schema into a model
