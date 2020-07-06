7f4c69a3e3e2ae9d7c2255b07305bbf3cdbbc76b
emvh <emilyvho@gmail.com>
2020-07-06 01:03:51 -0700

    Update webpack mode to production

--- webpack.config.js
+++ webpack.config.js
@@ -16,34 +16,15 @@ module.exports = {
           },
         },
       },
-      // {
-      //   test: /\.svg$/,
-      //   use: [{
-      //     loader: 'react-svg-loader',
-      //   }],
-      // },
-      // {
-      //   test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
-      //   loader: 'file-loader',
-      // },
-      // {
-      //   test: /\.svg$/,
-      //   use: ['@svgr/webpack', 'url-loader'],
-      // },
-      // // {
-      // //   test: /\.(svg|png|jpg|gif)$/,
-      // //   use: {
-      // //     loader: "file-loader",
-      // //     options: {
-      // //       name: "[name].[hash].[ext]",
-      // //       outputPath: "imgs"
-      // //     }
-      // //   }
-      // // },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
     ],
   },
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
-  }
+  },
+  mode: 'production',
 };
