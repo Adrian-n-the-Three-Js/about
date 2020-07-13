b4c0cb06bcd3909ab6ace749fa0e173648bdaa88
Justin Kwan <jkwan19@gmail.com>
2020-07-13 10:07:43 -0700

    Cassandra seed script works

--- database/database-primary/seedPhotos.js
+++ database/database-primary/seedPhotos.js
@@ -1,22 +1,27 @@
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
+const moment = require('moment');
 let writer = csvWriter();
 let faker = require('faker');
 
-let albums = ['room', 'pool', 'gym', 'dining', 'amenities', 'bathroom', 'eventroom', 'roomview'];
-let albumName = Math.random(albums.length)
+let albums = ['Room', 'Pool', 'Gym', 'Dining', 'Amenities', 'Bathroom', 'Eventroom', 'Roomview'];
 const generatePhotos = () => {
   writer.pipe(fs.createWriteStream('./data/photos.csv'))
   for (let id = 1; id <= 10; id++) {
+    let randomAlbum = Math.floor(Math.random() * (albums.length - 0));
+    let date = moment(faker.date.past()).format('MMM D');
     writer.write({
-      photo_id: id,
       hotel_id: faker.random.number({min: 1, max: 10000000}),
-      category: albums[albumName],
+      category: albums[randomAlbum],
+      photo_id: id,
+      avatar_url: faker.image.imageUrl(),
       caption: faker.lorem.sentence(),
-      image_url: faker.image.imageUrl(),
-      user_id: faker.random.number({min: 1, max: 10000000}),
-      date_posted: faker.date.past(),
-      helpful_votes: faker.random.number({min: 0, max: 30})
+      contributions: faker.random.number({min: 0, max: 100}),
+      date_posted: date,
+      helpful_votes: faker.random.number({min: 0, max: 30}),
+      location: faker.address.city(),
+      photo_url: faker.image.imageUrl(),
+      user: faker.name.findName()
     })
   }
   writer.end()
